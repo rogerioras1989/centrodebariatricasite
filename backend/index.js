f5b02e2a1a2f59df4ee029f22ea6b10b6082dd82
Rogério Santana <rogerio.ras1989@gmail.com>
2025-01-13 22:35:32 -0300

    Alterando a porta do backend para 3001

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const ImcData = require('./ImcData');
 
 // Inicializa o aplicativo
 const app = express();
-const PORT = process.env.PORT || 3000;;
+const PORT = process.env.PORT || 3001;;
 
 // Conexão com o MongoDB Atlas
 mongoose
